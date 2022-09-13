fe7e60f64aec1a6945727b3dd8c05554202aa554
Yooo31 <mrts.yoan@gmail.com>
2022-09-13 22:38:02 +0200

    Create discord command

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ import os
 
 from dotenv import load_dotenv
 import discord
+from discord.ext import commands
 
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-client.run(os.getenv("TOKEN"))
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+  print("Conection Ok!")
+
+@bot.command(name='r')
+async def lunch_royaltiz(ctx):
+  await ctx.channel.send("Royaltiz")
+
+@bot.command(name='igraal')
+async def lunch_royaltiz(ctx):
+  await ctx.channel.send("Royaltiz")
+
+
+bot.run(os.getenv("TOKEN"))
